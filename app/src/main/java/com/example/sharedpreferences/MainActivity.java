eb612827d5a4eb08af2934c5d6c22eeba81c16ea
xkadmx <zuzanna.rosinska@yahoo.com>
2019-04-22 15:09:43 +0200

    binding widgets with MainActivity

--- app/src/main/java/com/example/sharedpreferences/MainActivity.java
+++ app/src/main/java/com/example/sharedpreferences/MainActivity.java
@@ -1,13 +1,27 @@
 package com.example.sharedpreferences;
 
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
 
 public class MainActivity extends AppCompatActivity {
 
+    SharedPreferences utils;
+    Button buttonOk;
+    EditText editLogin;
+
+
+    @SuppressLint("WrongConstant")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        utils = getSharedPreferences("loginPref", MODE_PRIVATE);
     }
 }
